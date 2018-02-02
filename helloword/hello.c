b5eea25ba0cae8f5597baf6608965a6f6b2e1455
Gang Deng <Gang_Deng@sigmadesigns.com>
2018-02-02 13:35:42 +0800

    [XYZ-000] can see messag in dmesg
    [Problem]
     - Must add  LDDINC=$(PWD)/../include in makefile
    I don't know why
    
    [Solution]
     -
    
    [ImpactDomain]
     -

--- helloword/hello.c
+++ helloword/hello.c
@@ -1,22 +1,26 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
+
+MODULE_AUTHOR("Alessandro Rubini, Jonathan Corbet");
 MODULE_LICENSE("Dual BSD/GPL");
+
 
 int hello_init(void)
 {
-	printk(KERN_EMERG"Hello world!\n");
+    printk(KERN_NOTICE "hello_init\n");
 	return 0;
 
 }
 
 void hello_exit(void)
 {
-	printk(KERN_EMERG "GOODBY ,cruel world\n");
+    printk(KERN_NOTICE "hello_exit\n");
 }
 
 
 module_init(hello_init);
 module_exit(hello_exit);
+
 
 
