7c4642cbd0841555cb8aadbf6c08a50a1a3b1a63
Gang Deng <Gang_Deng@sigmadesigns.com>
2018-02-02 17:45:41 +0800

    [XYZ-000] allocate device major ID
    [Problem|Feature|Improvement]
     -
    
    [Solution]
     -
    
    [ImpactDomain]
     -

--- helloword/hello_main.c
+++ helloword/hello_main.c
@@ -1,14 +1,26 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/fs.h>
 
 
 MODULE_AUTHOR("Alessandro Rubini, Jonathan Corbet");
 MODULE_LICENSE("Dual BSD/GPL");
 
+static int hello_major;
+static int hello_minor = 3;
+static int hello_devs = 4;
 
-int hello_init(void)
+int __init hello_init(void)
 {
+    int result;
+    dev_t dev= 0;
+    
     printk(KERN_NOTICE "hello_init\n");
+    result = alloc_chrdev_region(&dev, hello_minor, hello_devs,
+            "hello");
+    hello_major = MAJOR(dev);
+    printk(KERN_EMERG "hello_major = %d\n",hello_major);
+
 	return 0;
 
 }
@@ -16,6 +28,9 @@ int hello_init(void)
 void hello_exit(void)
 {
     printk(KERN_NOTICE "hello_exit\n");
+    dev_t dev=0;
+    dev=MKDEV(hello_major,hello_minor);
+    unregister_chrdev_region(dev,hello_devs);
 }
 
 
